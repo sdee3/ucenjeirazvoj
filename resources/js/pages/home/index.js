d0d9ccf8b641dc79674bc3d32e5cbac2bc4d6859
Stefan <stefandjokic@pm.me>
2020-05-20 11:16:40 +0200

    Changed cover image on Homepage

--- resources/js/pages/home/index.js
+++ resources/js/pages/home/index.js
@@ -8,14 +8,7 @@ export default function Home() {
       <section className='home-page__main-img'>
         <div className='container'>
           <div className='heading'>
-            <div className='image-section'>
-              <img
-                alt='Učenje i razvoj logo'
-                src='https://res.cloudinary.com/sdee3-com/image/upload/e_colorize,co_rgb:6574cd/v1589892083/ucenjeirazvoj/tree.png'
-              />
-              <p>Učenje i razvoj</p>
-            </div>
-            <h1>Da budemo bolja verzija sebe.</h1>
+            <div className='image-section'></div>
           </div>
         </div>
       </section>
