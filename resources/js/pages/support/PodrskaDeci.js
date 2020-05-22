e0690de545574bac6e947599d810d92d2e02bbf1
Stefan <stefandjokic@pm.me>
2020-05-22 10:45:35 +0200

    Breadcrumbs on Support pages

--- resources/js/pages/support/PodrskaDeci.js
+++ resources/js/pages/support/PodrskaDeci.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { lazy } from "react";
+import { Link } from "react-router-dom";
+
+const Breadcrumbs = lazy(() => import("../../components/Breadcrumbs"));
 
 export default function PodrskaDeci() {
   return (
-    <section className='support-page'>
-      <div className='container'>
-        <h1>Podrška deci</h1>
-        <p>
-          Svako dete se tokom odrastanja suočava sa različitim izazovima zbog
-          kojih povremeno može imati potrebu za stručnom podrškom.
-        </p>
-        <div className='support-page__image'>
-          <img
-            alt='Učenje i razvoj - Podrška deci'
-            src='https://res.cloudinary.com/sdee3-com/image/upload/v1589803502/ucenjeirazvoj/PodrskaDeciGrafika.png'
-          />
+    <>
+      <Breadcrumbs
+        page={
+          <>
+            <Link to='/podrska'>Podrška</Link>
+            <i className='material-icons'>keyboard_arrow_right</i>
+            <Link to={`/podrska/podrska-deci`}>Podrška deci</Link>
+          </>
+        }
+      />
+      <section className='support-page'>
+        <div className='container'>
+          <h1>Podrška deci</h1>
+          <p>
+            Svako dete se tokom odrastanja suočava sa različitim izazovima zbog
+            kojih povremeno može imati potrebu za stručnom podrškom.
+          </p>
+          <div className='support-page__image'>
+            <img
+              alt='Učenje i razvoj - Podrška deci'
+              src='https://res.cloudinary.com/sdee3-com/image/upload/v1589803502/ucenjeirazvoj/PodrskaDeciGrafika.png'
+            />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
--- resources/js/pages/support/PodrskaOdraslima.js
+++ resources/js/pages/support/PodrskaOdraslima.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { lazy } from "react";
+import { Link } from "react-router-dom";
+
+const Breadcrumbs = lazy(() => import("../../components/Breadcrumbs"));
 
 export default function PodrskaOdraslima() {
   return (
-    <section className='support-page'>
-      <div className='container'>
-        <h1>Podrška odraslima</h1>
-        <p>
-          Tokom života susrećemo se sa različitim izazovima koje da bi uspešno
-          rešili povremeno možemo imati potrebu za stručnom podrškom u cilju:
-        </p>
-        <div className='support-page__image'>
-          <img
-            alt='Učenje i razvoj - Podrška odraslima'
-            src='https://res.cloudinary.com/sdee3-com/image/upload/v1589805767/ucenjeirazvoj/PodrskaOdraslimaGrafika.png'
-          />
+    <>
+      <Breadcrumbs
+        page={
+          <>
+            <Link to='/podrska'>Podrška</Link>
+            <i className='material-icons'>keyboard_arrow_right</i>
+            <Link to={`/podrska/podrska-odraslima`}>Podrška odraslima</Link>
+          </>
+        }
+      />
+      <section className='support-page'>
+        <div className='container'>
+          <h1>Podrška odraslima</h1>
+          <p>
+            Tokom života susrećemo se sa različitim izazovima koje da bi uspešno
+            rešili povremeno možemo imati potrebu za stručnom podrškom u cilju:
+          </p>
+          <div className='support-page__image'>
+            <img
+              alt='Učenje i razvoj - Podrška odraslima'
+              src='https://res.cloudinary.com/sdee3-com/image/upload/v1589805767/ucenjeirazvoj/PodrskaOdraslimaGrafika.png'
+            />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
