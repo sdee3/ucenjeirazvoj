9da7cca9be51ba2117ed77d64e55211b5e650289
Stefan <stefandjokic@pm.me>
2020-06-13 12:05:51 +0200

    Changed home image #4

--- resources/js/pages/home/index.js
+++ resources/js/pages/home/index.js
@@ -1,46 +1,54 @@
 import React, { lazy } from "react";
 import { Helmet } from "react-helmet";
+import { useMediaQuery } from "react-responsive";
 
 const Services = lazy(() => import("./Services"));
 const Promo = lazy(() => import("./Promo"));
 
 export default function Home() {
+  const isMobile = useMediaQuery({ query: "(max-width: 769px)" });
+
   return (
     <>
       <Helmet>
         <title>Učenje i razvoj</title>
         <meta
-          name='description'
-          content='Učenje i razvoj - Da budemo bolja verzija sebe'
+          name="description"
+          content="Učenje i razvoj - Da budemo bolja verzija sebe"
         />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://ucenjeirazvoj.com/' />
-        <meta property='og:title' content='Učenje i razvoj' />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ucenjeirazvoj.com/" />
+        <meta property="og:title" content="Učenje i razvoj" />
         <meta
-          property='og:description'
-          content='Učenje i razvoj - Da budemo bolja verzija sebe'
-        />
-        <meta
-          property='og:image'
-          content='https://res.cloudinary.com/sdee3-com/image/upload/v1589966037/ucenjeirazvoj/ucenjeirazvoj-main-txt.jpg'
-        />
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:url' content='https://ucenjeirazvoj.com/' />
-        <meta property='twitter:title' content='Učenje i razvoj' />
-        <meta
-          property='twitter:description'
-          content='Učenje i razvoj - Da budemo bolja verzija sebe'
+          property="og:description"
+          content="Učenje i razvoj - Da budemo bolja verzija sebe"
         />
         <meta
-          property='twitter:image'
-          content='https://res.cloudinary.com/sdee3-com/image/upload/v1589966037/ucenjeirazvoj/ucenjeirazvoj-main-txt.jpg'
+          property="og:image"
+          content="https://res.cloudinary.com/sdee3-com/image/upload/v1592039530/ucenjeirazvoj/ucenje-i-razvoj-cover.jpg"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://ucenjeirazvoj.com/" />
+        <meta property="twitter:title" content="Učenje i razvoj" />
+        <meta
+          property="twitter:description"
+          content="Učenje i razvoj - Da budemo bolja verzija sebe"
+        />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/sdee3-com/image/upload/v1592039530/ucenjeirazvoj/ucenje-i-razvoj-cover.jpg"
         />
       </Helmet>
-      <section className='home-page'>
-        <section className='home-page__main-img'>
-          <div className='container'>
-            <h1>Da budemo bolja verzija sebe.</h1>
-          </div>
+      <section className="home-page">
+        <section className="home-page__main-img">
+          <img
+            alt="Učenje i razvoj cover"
+            src={
+              isMobile
+                ? "https://res.cloudinary.com/sdee3-com/image/upload/v1592039529/ucenjeirazvoj/ucenje-i-razvoj-cover-min.jpg"
+                : "https://res.cloudinary.com/sdee3-com/image/upload/v1592042661/ucenjeirazvoj/ucenje-i-razvoj-cover-max.png"
+            }
+          />
         </section>
         <Services />
         <Promo />
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,14 +17,14 @@
   <meta property='og:url' content='https://ucenjeirazvoj.com/' />
   <meta property='og:title' content='Učenje i razvoj' />
   <meta property='og:description' content='Učenje i razvoj - Da budemo bolja verzija sebe' />
-  <meta property='og:image'
-    content='https://res.cloudinary.com/sdee3-com/image/upload/v1589966037/ucenjeirazvoj/ucenjeirazvoj-main-txt.jpg' />
+  <meta property='og:image' content='https://res.cloudinary.com/sdee3-com/image/upload/v1592039530/ucenjeirazvoj/ucenje-i-razvoj-cover.jpg
+    ' />
   <meta property='twitter:card' content='summary_large_image' />
   <meta property='twitter:url' content='https://ucenjeirazvoj.com/' />
   <meta property='twitter:title' content='Učenje i razvoj' />
   <meta property='twitter:description' content='Učenje i razvoj - Da budemo bolja verzija sebe' />
   <meta property='twitter:image'
-    content='https://res.cloudinary.com/sdee3-com/image/upload/v1589966037/ucenjeirazvoj/ucenjeirazvoj-main-txt.jpg' />
+    content='https://res.cloudinary.com/sdee3-com/image/upload/v1592039530/ucenjeirazvoj/ucenje-i-razvoj-cover.jpg' />
 
   @if (env('APP_ENV') === 'production')
   <!-- Trackers for production -->
